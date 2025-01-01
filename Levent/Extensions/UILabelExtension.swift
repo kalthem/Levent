5137d1c78183c33fcb4298b070920f4c4d13b08d
kalthem <202103011@student.polytechnic.bh>
2025-01-01 15:31:22 +0300

    Delete Levent directory

--- Levent/Extensions/UILabelExtension.swift
+++
@@ -1,8 +0,0 @@
-
-import UIKit
-extension UILabel {
-    /// Sets the text with the first letter of each word capitalized.
-    func setCapitalizedText(_ text: String) {
-        self.text = text.capitalized
-    }
-}
